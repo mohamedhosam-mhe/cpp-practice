4f549485ad43ff295f051852370bf944b2edb7ca
Mohamed Hosam <mhe.mohamedhosam@gmail.com>
2026-04-07 10:50:07 +0400

    Rename folder and add problems 41-45

---
+++ Problems 41 to 45/Problems 41 to 45.cpp
@@ -0,0 +1,27 @@
+// Problems 41 to 45.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+
+
+
+
+
+int main()
+{
+
+
+
+
+
+
+
+
+
+}
+
